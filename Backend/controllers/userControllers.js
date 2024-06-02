de35c54f7f1472cc1a8503940946df02306a27dd
karthik-140 <shanigaramkarthik@gmail.com>
2024-06-02 16:55:53 +0530

    added security for password by encrypting using bcrypt library

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -1,17 +1,30 @@
 const users = require('../models/users');
+const bcrypt = require('bcrypt');
+
+const isNotValidString = (value) => {
+  return !!(value === undefined || value.length === 0);
+}
 
 exports.singupUser = (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
-    users.create({ name: name, email: email, password: password })
-      .then((result) => {
-        console.log('User sing-up succesful');
-        res.status(201).json({ message: 'User sign-up successful', result });
-      }).catch(err => {
-        console.log('Email already exists!!', err);
-        res.status(500).json({ message: 'Email already exists!!', details: err });
-      })
+    if (isNotValidString(name) || isNotValidString(email) || isNotValidString(password)) {
+      return res.status(400).json({ message: 'Bad parameters!!' });
+    }
+
+    const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+      console.log('hashErr', err);
+      users.create({ name: name, email: email, password: hash })
+        .then((result) => {
+          console.log('User sing-up succesful');
+          res.status(201).json({ message: 'User sign-up successful', result });
+        }).catch(err => {
+          console.log('Email already exists!!', err);
+          res.status(500).json({ message: 'Email already exists!!', details: err });
+        })
+    })
 
   } catch (err) {
     console.log('Error in signupUser function', err);
@@ -26,11 +39,18 @@ exports.loginUser = async (req, res, next) => {
 
     if (user === null) {
       res.status(404).json({ message: 'User not found!!' })
-    } else if (user.password !== password) {
-      res.status(401).json({ message: 'Password is Incorrect!!' })
-    } else {
-      res.status(201).json({ message: 'User login successful', response: user })
     }
+
+    bcrypt.compare(password, user.password, (err, response) => {
+      if (err) {
+        res.status(500).json({ message: 'something went wrong!!' });
+      }
+      if (response) {
+        res.status(200).json({ message: 'User login successful', response: user })
+      } else {
+        res.status(400).json({ message: 'Password is Incorrect!!' })
+      }
+    })
 
   } catch (err) {
     console.log('Server Error!!', err)
